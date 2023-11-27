3bf1774eef8af7418a7e1b2db94128b6d9b6cc89
unknown <=>
2023-11-27 23:07:58 +0300

    DDL and DML querys updated

---
+++ DML.sql
@@ -0,0 +1,9 @@
+Select ContactName Adi,CompanyName SirketAdi,City Sehir from Customers
+
+SELECT * FROM customers WHERE City = 'Berlin';
+
+INSERT INTO products (ProductName, UnitPrice) VALUES ('Phone', 2000.00);
+
+UPDATE employees SET FirstName = 'Ramazan' WHERE LastName = 'Robert';
+
+DELETE FROM products WHERE ProductName = 'Phone';
