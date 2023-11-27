12e858328b374c7c7faa32616155696b50a7d638
unknown <=>
2023-11-27 23:09:28 +0300

    DML and DDL querys Updated

---
+++ DDL.sql
@@ -0,0 +1,16 @@
+CREATE TABLE productss (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(255),
+    category_id INT,
+    price DECIMAL(10, 2),
+    in_stock BOOLEAN
+);
+
+ALTER TABLE customers ADD email VARCHAR(255);
+
+ALTER TABLE customers DROP COLUMN email;
+
+CREATE INDEX idx_product_categoryY ON products(category_id);
+
+CREATE VIEW affordable_products AS
+SELECT * FROM products WHERE UnitPrice < 50.00;
--- DML.sql
+++ DML.sql
@@ -1,3 +1,6 @@
+DDL SORGULARI
+
+
 Select ContactName Adi,CompanyName SirketAdi,City Sehir from Customers
 
 SELECT * FROM customers WHERE City = 'Berlin';
@@ -7,3 +10,4 @@ INSERT INTO products (ProductName, UnitPrice) VALUES ('Phone', 2000.00);
 UPDATE employees SET FirstName = 'Ramazan' WHERE LastName = 'Robert';
 
 DELETE FROM products WHERE ProductName = 'Phone';
+
